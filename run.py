c44b13e568d512e356b12e4be9bdc348857e7201
Raxxius <Raxxman@gmail.com>
2021-12-27 12:51:14 +0000

    finished deck function

--- run.py
+++ run.py
@@ -1,18 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
 suite = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
-deck_of_cards = ["hearts", "diamonds", "clubs", "spades"]
+deck_of_cards = ["Hearts", "Diamonds", "Clubs", "Spades"]
 
 def deck():
     deck = []
     number_of_packs = 4
-    for house in deck_of_cards:
-        for card in suite:
-            deck.append(f"{house} {card}")
-    print(deck)
-    print(len(deck))
+    for i in range(number_of_packs):
+        for house in deck_of_cards:
+            for card in suite:
+                deck.append(f"{house} {card}")
+    random.shuffle(deck)
+    return deck
 
 deck()
 
