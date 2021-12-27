763c9bdad1362bbda2ca024d3aedb2d789a3b2c6
Raxxius <Raxxman@gmail.com>
2021-12-27 12:04:22 +0000

    add deck() function

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+suite = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+deck_of_cards = ["hearts", "diamonds", "clubs", "spades"]
+
+def deck():
+    deck = []
+    number_of_packs = 4
+    for house in deck_of_cards:
+        for card in suite:
+            deck.append(f"{house} {card}")
+    print(deck)
+    print(len(deck))
+
+deck()
+
