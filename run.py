d6d8306dbec8b9e54a4ce0820855f6fa496c38e4
Raxxius <Raxxman@gmail.com>
2021-12-28 11:02:42 +0000

    saved updated draw function

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 # from art import card
 
+
 def mixs(num):
+    # Sorts a mixed list, Int first (low to high) then Str (low to high) 
     try:
         ele = int(num)
         return (1, ele, '')
     except ValueError:
         return (0, num, '')
+
 
 def deck():
     # Creates the deck of cards based on 4 packs of cards 
@@ -24,33 +24,32 @@ def deck():
     random.shuffle(deck)
     return deck
 
+
 def card_sum(player_cards):
+    # Adds the total of the cards in a hand
+    # Sorts so that Aces are counted last to allow for
+    # The two potential values of the Ace.
     player_total = 0
-    print(player_total)
-    print(player_cards)
     player_cards.sort(key = mixs, reverse = True)
-    print(player_cards)
     for card in player_cards:
         print(card)
         if card == "Ace":
             if player_total + 11 < 22:
                 player_total += 11
-                print(player_total)
             else: player_total += 1
-            print(player_total)
         elif card in ["Jack", "King", "Queen"]:
             player_total += 10
-            print(player_total)
         else:
             print(card)
             player_total += card
-            print(player_total)
-    print(f"Player total score = {player_total}")
+    return player_total
 
 
 def draw(deck):
+    player_card = deck.pop(len(deck)-1)
+    return player_card, deck
 
-    player_turn_end = False
+"""    player_turn_end = False
     player_cards = []
     player_cards.append(deck.pop(len(deck)-1))
     card_number = len(player_cards)
@@ -62,7 +61,10 @@ def draw(deck):
             player_card_1 = deck.pop(len(deck)-1)
             print(player_card_1)
             card_number += 1
-            player_turn_end == True
+            player_turn_end == True"""
+    
 
-player_cards = ["Queen", 3, "Ace", 7]
-card_sum(player_cards)
+deck = deck()
+player_card, deck = draw(deck)
+print(player_card)
+print(deck)
